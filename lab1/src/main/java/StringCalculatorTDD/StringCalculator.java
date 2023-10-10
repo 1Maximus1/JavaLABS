d6d3304121c6ade4798c738be13180c0f9d90313
Oleh <elohhh2@gmail.com>
2023-10-10 22:05:25 +0300

    Step_8

--- lab1/src/main/java/StringCalculatorTDD/StringCalculator.java
+++ lab1/src/main/java/StringCalculatorTDD/StringCalculator.java
@@ -2,63 +2,102 @@ package StringCalculatorTDD;
 
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
 	public int add(String numbers) {
 		ArrayList<Integer> negativeNumbers = new ArrayList<>();
+		ArrayList<String> allDelimiters = new ArrayList<>();
+		String delimiter = "[,\\n]";
 
 		if (numbers.isEmpty()) {
 			return 0;
 		}
 
-		if (numbers.startsWith("//[")){
+		checkCharactersStartAndEnd(numbers);
+		throwCustomDelimeterInvalidInputException(numbers);
+		if (numbers.startsWith("//["))
+		{
 			numbers = numbers.replaceAll("\n", ",");
+		} else if (numbers.startsWith("//]")) {
+			throw new IllegalArgumentException("Error: Incorrect custom delimiter!");
 		}
 
-		String delimiter = "[,\\n]";
 
-		if (numbers.startsWith("//"))
+		Matcher matcher = Pattern.compile("\\[(.*?)\\]").matcher(numbers);
+
+		String[] numbersArrayFin;
+		if (checkCharactersStartAndEnd(numbers))
 		{
-			Matcher matcher = Pattern.compile("\\[(.*?)\\](\\n|,)").matcher(numbers);
-			if (matcher.find()) {
-				delimiter = Pattern.quote(matcher.group(1));
-				numbers = numbers.substring(matcher.end());
-			} else {
-				delimiter = String.valueOf(numbers.charAt(2));
-				numbers = numbers.substring(3);
+
+
+			while (matcher.find())
+			{
+				String delimiter2 = Pattern.quote(matcher.group(1));
+				allDelimiters.add(delimiter2);
+			}
+
+			if (!allDelimiters.isEmpty()){
+				Collections.sort(allDelimiters, new Comparator<String>() {
+					@Override
+					public int compare(String o1, String o2) {
+						if(o1.length()>o2.length()){
+							return -1;
+						}
+						if (o1.length()<o2.length()){
+							return 1;
+						}
+						return 0;
+					}
+				});
+			}
+
+
+
+			Matcher matcher0 = Pattern.compile("\\[(.*?)\\](\\n|,)").matcher(numbers);
+			if (matcher0.find()) {
+				numbers = numbers.substring(matcher0.end());
+			}
+
+
+			if (!allDelimiters.isEmpty() && allDelimiters.toArray().length > 1)
+			{
+				for (String allDelimiter : allDelimiters) {
+					numbers = numbers.replaceAll(allDelimiter, ",");
+				}
+
+				numbersArrayFin = numbers.split(",");
+				checkDuplicateOfCommas(numbers);
+				return sumOfStrings(numbersArrayFin,negativeNumbers);
+
+
+			} else if (allDelimiters.toArray().length == 1) {
+
+				for (String allDelimiter : allDelimiters) {
+					numbers = numbers.replaceAll(allDelimiter, ",");
+				}
+
+			}else{
+				String delimiter2 = Pattern.quote(String.valueOf(numbers.charAt(2)));
+				numbers = numbers.substring(4);
+				checkCharactersStartAndEnd(numbers);
+				numbers = numbers.replaceAll("\n",",");
+				numbers = numbers.replaceAll(delimiter2,",");
+				checkDuplicateOfCommas(numbers);
 			}
 		}
 
-		if (numbers.contains(",\n") || numbers.contains("\n,")) {
+		if (numbers.contains(",\n") || numbers.contains("\n,") || numbers.contains(",,") || numbers.contains("\n\n")) {
 			throw new IllegalArgumentException("Error: Two delimiters are following each other!");
 		}
 
+		checkCharactersStartAndEnd(numbers);
 		String[] numbersArray = numbers.split(delimiter);
 
-
-		int sum = 0;
-
-		for (String number : numbersArray) {
-			if (!number.trim().isEmpty()) {
-				int num;
-				try {
-					num = Integer.parseInt(number.trim());
-				}catch (IllegalArgumentException e){
-					throw new IllegalArgumentException("Error: Two delimiters are following each other!");
-				}
-				if (num < 0) {
-					negativeNumbers.add(num);
-				} else if (num > 1000) {
-					continue;
-				}
-				if (negativeNumbers.isEmpty()) {
-					sum += num;
-				}
-
-			}
-		}
+		int sum = sumOfStrings(numbersArray,negativeNumbers);
 
 		if (negativeNumbers.isEmpty()) {
 			return sum;
@@ -74,6 +113,95 @@ public class StringCalculator {
 		}
 
 	}
+
+	public static int sumOfStrings(String[] stringNumbersArray, ArrayList<Integer> negativeNumbers){
+		int sum = 0;
+		for (String number : stringNumbersArray) {
+			if (!number.trim().isEmpty()) {
+				int num;
+				try {
+					num = Integer.parseInt(number.trim());
+				}catch (IllegalArgumentException e){
+					throw new IllegalArgumentException("Error: No specified delimiter exists!");
+				}
+				if (num < 0) {
+					negativeNumbers.add(num);
+				} else if (num > 1000) {
+					continue;
+				}
+				if (negativeNumbers.isEmpty()) {
+					sum += num;
+				}
+
+			}
+		}
+		return sum;
+	}
+	public static void checkDuplicateOfCommas(String numbers) {
+		int counterOfDuplicate = 0;
+		for (int i = 0; i < numbers.length(); i++) {
+			if(numbers.charAt(i) != ','){
+				counterOfDuplicate = 0;
+			}else{
+				counterOfDuplicate++;
+			}
+			if (counterOfDuplicate >= 2) {
+				throw new IllegalArgumentException ("Error: Two delimiters are following each other!");
+			}
+		}
+
+	}
+
+	public static boolean checkCharactersStartAndEnd(String numbers) {
+		if (numbers.startsWith("/")) {
+			if (numbers.startsWith("//")){
+				return true;
+			}else {
+				throw new IllegalArgumentException("Error: Incorrect input!(Expression starts with \"//\" by template)");
+			}
+		}
+
+		if (!numbers.startsWith("//")) {
+			if ((!Character.isDigit(numbers.charAt(0)) && !Character.isWhitespace(numbers.charAt(0))) || numbers.charAt(0) == '\n') {
+				throw new IllegalArgumentException("Error: Incorrect input(check template)!");
+			}
+
+			char lastChar = numbers.charAt(numbers.length() - 1);
+			if ((!Character.isDigit(lastChar) && !Character.isWhitespace(lastChar)) || lastChar == '\n') {
+				throw new IllegalArgumentException("Error: Incorrect input(ends with delimiter)!");
+			}
+		}else{
+			if ((!Character.isDigit(numbers.charAt(0)) && !Character.isWhitespace(numbers.charAt(0))) || numbers.charAt(0) == '\n') {
+				throw new IllegalArgumentException("Error: Incorrect input(starts with delimiter)!");
+			}
+			char lastChar = numbers.charAt(numbers.length() - 1);
+			if ((!Character.isDigit(lastChar) && !Character.isWhitespace(lastChar)) || lastChar == '\n') {
+				throw new IllegalArgumentException("Error: Incorrect input(ends with delimiter)!");
+			}
+		}
+
+		return false;
+	}
+	public void throwCustomDelimeterInvalidInputException(String input) {
+		if (input.startsWith("//[")) {
+			Pattern patternT1 = Pattern.compile("\\[([^\\]]+)\\]");
+			Matcher matcherT1 = patternT1.matcher(input);
+			Pattern patternT2 = Pattern.compile("\\[([^\\]]+)\\]");
+			Matcher matcherT2 = patternT2.matcher(input);
+			while (matcherT1.find()) {
+				String delimiterT = matcherT1.group(1);
+				if (delimiterT.contains("[") || delimiterT.contains("]")) {
+					throw new IllegalArgumentException("Error: Invalid delimiter format");
+				}
+			}
+			if(!matcherT2.find()){
+				throw new IllegalArgumentException("Error: Invalid delimiter format");
+			}
+
+		}
+	}
+
+
 }
 
 
--- lab1/src/test/java/StringCalculatorTest.java
+++ lab1/src/test/java/StringCalculatorTest.java
@@ -1,132 +1,217 @@
 import StringCalculatorTDD.StringCalculator;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StringCalculatorTest
 {
+	private StringCalculator stringCalculator;
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		stringCalculator = new StringCalculator();
+	}
 
 	@Test
 	public void testEmptyString() {
-		StringCalculator calculator = new StringCalculator();
-		int result = calculator.add("");
+		int result = stringCalculator.add("");
 		assertEquals(0, result);
 	}
 
 	@Test
 	public void testSingleNumber() {
-		StringCalculator calculator = new StringCalculator();
-		int result = calculator.add("5");
+		
+		int result = stringCalculator.add("5");
 		assertEquals(5, result);
 	}
 
-
 	@Test
 	public void testTwoNumbers() {
-		StringCalculator calculator = new StringCalculator();
-		int result = calculator.add("3,7,4,5");
-		assertEquals(19, result);
+		int result = stringCalculator.add("3,7");
+		assertEquals(10, result);
 	}
 
-
 	@Test
-	public void testIllegalArgumentException() {
-		StringCalculator calculator = new StringCalculator();
-		String input = "1,\n2";
-		assertThrows(IllegalArgumentException.class, () -> calculator.add(input));
+	public void testTwoNumbersWithSpaces(){
+		assertEquals(160, stringCalculator.add(" 60 , 100"));
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"66,8,9,4,12,25,1", "41,41\n41,2", "100, 20\n1\n1,1\n 1,1"})
+	public void testManyNumbersWithSpacesShiftsCommas(String input){
+		assertEquals(125, stringCalculator.add(input));
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"1\n\n2", "1\n,2", "1,\n2", "1,,2"})
+	public void throwExceptionOnTwoDelimiterInARow(String input){
+		Exception exception = assertThrows(IllegalArgumentException.class,
+				() -> stringCalculator.add(input));
+		assertEquals("Error: Two delimiters are following each other!", exception.getMessage());
 	}
 
 	@Test
 	public void testMultipleNumbers() {
-		StringCalculator calculator = new StringCalculator();
-		int result = calculator.add("1\n2,3,4\n5");
+		int result = stringCalculator.add("1\n2,3,4\n5");
 		assertEquals(15, result);
 	}
 
 	@Test
 	public void testMultipleNumbersWithDotAndCommaAndSpecifiedSeparator() {
-		StringCalculator calculator = new StringCalculator();
-		int result = calculator.add("//;\n1;2");
+		int result = stringCalculator.add("//;\n1;2");
 		assertEquals(3, result);
 	}
 
 	@Test
-	public void testMultipleNumbersWithSpacesAndSpecifiedSeparator() {
-		StringCalculator calculator = new StringCalculator();
-		int result = calculator.add("//\n\n1\n2\n3\n50");
+	public void testMultipleNumbersWithShiftsAndSpecifiedSeparator() {
+		int result = stringCalculator.add("//\n\n1\n2\n3\n50");
 		assertEquals(56, result);
 	}
 
 	@Test
-	public void testMultipleNegativeNumbers() {
-		StringCalculator calculator = new StringCalculator();
-		String input = ("-1,-3,-4,-5,3,1");
-		assertThrows(IllegalArgumentException.class, () -> calculator.add(input));
+	public void testMultipleNumbersWithQuestionMarkAndSpecifiedDelimiter() {
+		int result = stringCalculator.add("//?\n1?2?3\n50,23");
+		assertEquals(79, result);
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"//;\n;1;2", ",1\n2", "\n1,2", "1+2","/;\n1;2"})
+	public void throwIllegalArgumentExceptionMultipleNumbersWithSpecifiedDelimiterAndIncorrectInput(String input){
+		assertThrows(IllegalArgumentException.class, () -> stringCalculator.add(input));
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"//;\n;1;2", "//?\n?1?2?2", "//\n\n\n\n\n1\n\n2"})
+	public void throwIllegalArgumentExceptionMultipleNumbersWithSpecifiedDelimiterAndIncorrectInput_Text(String input){
+		assertThrows(IllegalArgumentException.class, () -> stringCalculator.add(input));
+		Exception exception = assertThrows(IllegalArgumentException.class,
+				() -> stringCalculator.add(input));
+		assertEquals("Error: Incorrect input(check template)!", exception.getMessage());
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"//;\n1;2;", "//?\n1?2?2?", "//A\n1A2A"})
+	public void throwIllegalArgumentExceptionMultipleNumbersWithSpecifiedDelimiter_Text(String input){
+		assertThrows(IllegalArgumentException.class, () -> stringCalculator.add(input));
+		Exception exception = assertThrows(IllegalArgumentException.class,
+				() -> stringCalculator.add(input));
+		assertEquals("Error: Incorrect input(ends with delimiter)!", exception.getMessage());
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"//;;\n1;;22;;1", "//??\n1??2??2??", "//AA\n1AA2AA"})
+	public void throwIllegalArgumentExceptionMultipleNumbersWithRepeatedSpecifiedDelimiter_Text(String input){
+		assertThrows(IllegalArgumentException.class, () -> stringCalculator.add(input));
+		Exception exception = assertThrows(IllegalArgumentException.class,
+				() -> stringCalculator.add(input));
+		assertEquals("Error: Incorrect input(check template)!", exception.getMessage());
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"/;\n1;22;1", "/?\n1?2?2?", "/A\n1A2A"})
+	public void throwIllegalArgumentExceptionMultipleNumbersWithIncorrectInput_Text(String input){
+		assertThrows(IllegalArgumentException.class, () -> stringCalculator.add(input));
+		Exception exception = assertThrows(IllegalArgumentException.class,
+				() -> stringCalculator.add(input));
+		assertEquals("Error: Incorrect input!(Expression starts with \"//\" by template)", exception.getMessage());
 	}
 
 	@Test
-	public void testMultipleNegativeNumbersWithSpaces() {
-		StringCalculator calculator = new StringCalculator();
-		String input = ("1\n-3\n-4,-5,3,1");
-		assertThrows(IllegalArgumentException.class, () -> calculator.add(input));
+	public void testMultipleNegativeNumbers() {
+		String input = ("-1,-3,-4,-5,3,1");
+		assertThrows(IllegalArgumentException.class, () -> stringCalculator.add(input));
 	}
+
+	@Test
+	public void testMultipleNegativeNumbersWithShifts() {
+		String input = ("1\n-3\n-4,-5,3,1");
+		assertThrows(IllegalArgumentException.class, () -> stringCalculator.add(input));
+	}
+
 	@Test
 	public void testMultipleNegativeNumbersAndSpecifiedSeparator() {
-		StringCalculator calculator = new StringCalculator();
 		String input = ("//\n\n1\n-12\n3\n-50");
-		assertThrows(IllegalArgumentException.class, () -> calculator.add(input));
+		assertThrows(IllegalArgumentException.class, () -> stringCalculator.add(input));
 	}
 
 	@Test
 	public void testMultipleNumbersWith1000() {
-		StringCalculator calculator = new StringCalculator();
-		int result = calculator.add("999,1000,1001,100000");
+		int result = stringCalculator.add("999,1000,1001,100000");
 		assertEquals(1999, result);
 	}
+
 	@Test
 	public void testMultipleNumbersAndSpecifiedSeparatorWith1000() {
-		StringCalculator calculator = new StringCalculator();
-		int result = calculator.add("//\n\n1\n12\n3\n50\n1000\n10012");
+		int result = stringCalculator.add("//^\n1^12^3^50^1000^10012");
 		assertEquals(1066, result);
 	}
 
 	@Test
 	void testCustomDelimiterThatConsistOfThreeCharacters() {
-		StringCalculator calculator = new StringCalculator();
-		int actual = calculator.add("//[+++]\n5+++3+++6");
-		Assertions.assertEquals(14, actual);
+		int result = stringCalculator.add("//[+++]\n5+++3+++6");
+		Assertions.assertEquals(14, result);
+	}
+
+	@Test
+	void testCustomDelimiterThatConsistOfThreeCharacters_2() {
+		int result = stringCalculator.add("//[***]\n1***2***3");
+		Assertions.assertEquals(6, result);
 	}
 
 	@Test
 	void testCustomDelimiterThatConsistOfSixCharacters() {
-		StringCalculator calculator = new StringCalculator();
-		int actual = calculator.add("//[******]\n5******3******6******4");
-		Assertions.assertEquals(18, actual);
+		int result = stringCalculator.add("//[******]\n5******3******6******4");
+		Assertions.assertEquals(18, result);
 	}
+
 	@Test
-	void testCustomDelimiterThatConsistOfThreeCharacters2() {
-		StringCalculator calculator = new StringCalculator();
-		int actual = calculator.add("//[***]\n1***2***3");
-		Assertions.assertEquals(6, actual);
-	}
-	@Test
-	void testCustomDelimiterThatConsistOfTwoCharactersWithComma() {
-		StringCalculator calculator = new StringCalculator();
-		int actual = calculator.add("//[,,]\n10,,10,,10,,10");
-		Assertions.assertEquals(40, actual);
+	void testCustomDelimiterThatConsistOfTwoCharactersWithCommas() {
+		int result = stringCalculator.add("//[,,]\n10,,10,,10,,10");
+		Assertions.assertEquals(40, result);
 	}
 	@Test
 	void testCustomDelimiterThatConsistOfTwoCharactersWithShifts() {
-		StringCalculator calculator = new StringCalculator();
-		int actual = calculator.add("//[\n\n]\n10\n\n10\n\n10\n\n10");
-		Assertions.assertEquals(40, actual);
+		int result = stringCalculator.add("//[\n\n]\n10\n\n10\n\n10\n\n10");
+		Assertions.assertEquals(40, result);
 	}
+
+	@Test
+	void testCustomThreeDelimitersThatConsistOfTCharactersOneOrTwo() {
+		int result = stringCalculator.add("//[**][,][+]\n10**10,10+10");
+		Assertions.assertEquals(40, result);
+	}
+
+	@Test
+	void testCustomFourDelimitersThatConsistMultipleCharacters_1() {
+		int result = stringCalculator.add("//[***][\n\n][+][..]\n10***10\n\n10,10+1000..11..22\n\n11..22+1110");
+		Assertions.assertEquals(1106, result);
+	}
+
 	@Test
 	public void throwIllegalArgumentExceptionIfDelimiterFollowByAnotherDelimiter() {
-		StringCalculator calculator = new StringCalculator();
 		String input = ("//[++]\n4++18,5++4++,2");
-		assertThrows(IllegalArgumentException.class, () -> calculator.add(input));
+		assertThrows(IllegalArgumentException.class, () -> stringCalculator.add(input));
 	}
+
+	@Test
+	void throwIllegalArgumentExceptionIfDelimiterFollowByAnotherDelimiter_Text() {
+		Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+			stringCalculator.add("//[***][\n]\n4***18\n***5***4***2");
+		});
+
+		assertEquals("Error: Two delimiters are following each other!", exception.getMessage());
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"//[***][\n1,2", "//][AD]\n1,2", "//[[]]\n1,2","//[[[]]]\n1,2", "//[\n1,2,2,3", "//][[]\n1,2,2,3", "//]\n1,2,2,1,2,3"})
+	public void throwCustomIllegalArgumentExceptionIfDelimiterInvalidInput(String input){
+	assertThrows(IllegalArgumentException.class,() -> stringCalculator.add(input));
+	}
+
+
+
 }
